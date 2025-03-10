40493d618a913cf485c56dc2cb558d6cecfd5893
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-03-10 08:51:30 +0800

    removed getenv

--- src/main/java/com/ervingorospe/grab_api_gateway/GrabApiGatewayApplication.java
+++ src/main/java/com/ervingorospe/grab_api_gateway/GrabApiGatewayApplication.java
@@ -10,14 +10,14 @@ public class GrabApiGatewayApplication {
 	public static void main(String[] args) {
 		// Load .env file
 //		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("DB_URL", System.getenv("DB_URL"));
-		System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-		System.setProperty("DB_DRIVER", System.getenv("DB_DRIVER"));
-		System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
-		System.setProperty("JWT_expiration", System.getenv("JWT_expiration"));
-		System.setProperty("JWT_REFRESH_EXPIRATION", System.getenv("JWT_REFRESH_EXPIRATION"));
-		System.setProperty("PORT", System.getenv("PORT_API_GATEWAY"));
+//		System.setProperty("DB_URL", System.getenv("DB_URL"));
+//		System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+//		System.setProperty("DB_DRIVER", System.getenv("DB_DRIVER"));
+//		System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
+//		System.setProperty("JWT_expiration", System.getenv("JWT_expiration"));
+//		System.setProperty("JWT_REFRESH_EXPIRATION", System.getenv("JWT_REFRESH_EXPIRATION"));
+//		System.setProperty("PORT", System.getenv("PORT_API_GATEWAY"));
 
 		SpringApplication.run(GrabApiGatewayApplication.class, args);
 	}
