6abf9ef813307dd12768317ba0716b701bbb33d5
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-03-27 17:49:37 +0800

    added cors policy

--- src/main/java/com/ervingorospe/grab_api_gateway/config/ApiGatewayConfig.java
+++ src/main/java/com/ervingorospe/grab_api_gateway/config/ApiGatewayConfig.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @Configuration
 public class ApiGatewayConfig {
@@ -75,5 +80,19 @@ public class ApiGatewayConfig {
                         .uri(bookingServiceUrl)
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
